0edffc3a1a74181ab03eca70703b5445ad9c66e8
RZHHHN <RZHHHN@USAUSWNCN2K44BW.nam.corp.gm.com>
2015-05-22 16:38:39 -0500

    pojo bean test with properties file

--- springdemo/beanannotations/src/main/java/com/soni/beanannotations/simple/PojoBeanTest.java
+++ springdemo/beanannotations/src/main/java/com/soni/beanannotations/simple/PojoBeanTest.java
@@ -1,5 +1,6 @@
 package com.soni.beanannotations.simple;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,7 +10,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class PojoBeanTest {
     public static void main(String[] args) {
-        ApplicationContext ctx = new  ClassPathXmlApplicationContext("config1.xml");
+//        testWithConfig("simplepojo/config1.xml");
+        testWithConfig("simplepojo/config2.xml");
+    }
+
+    private static void testWithConfig(final String config) throws BeansException {
+        ApplicationContext ctx = new  ClassPathXmlApplicationContext(config);
         PojoBean pojo = (PojoBean)ctx.getBean("pojo1");
         System.err.println("spring bean test");
         System.err.println(pojo.getFirstName());
